c85e75507a63ba6841145c56b9f044e5f756bc35
Ahmad N. Gohar <ansgohar@gmail.com>
2025-09-17 14:57:13 -0700

    migrating the content of the old chapter 3 after adding AI chapter

--- chapter-03/inventory-ms/src/main/java/com/packt/bookstore/inventory/dto/BookDTO.java
+++
@@ -1,7 +0,0 @@
-package com.packt.bookstore.inventory.dto;
-
-import java.math.BigDecimal;
-
-public record BookDTO(Long id, String title, String authorName, BigDecimal price) {
-
-}
--- chapter-03/inventory-ms/src/main/java/com/packt/bookstore/inventory/dto/BookSummary.java
+++
@@ -1,9 +0,0 @@
-package com.packt.bookstore.inventory.dto;
-
-import java.math.BigDecimal;
-
-public interface BookSummary {
-    String getTitle();
-    BigDecimal getPrice();
-
-}
