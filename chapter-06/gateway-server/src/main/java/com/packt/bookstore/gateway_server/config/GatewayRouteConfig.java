b914f0d496318ff66b76d4d724739f986e27933e
Ahmad N. Gohar <ansgohar@gmail.com>
2025-10-12 14:16:40 -0700

    discovery and gateway

---
+++ chapter-06/gateway-server/src/main/java/com/packt/bookstore/gateway_server/config/GatewayRouteConfig.java
@@ -0,0 +1,43 @@
+package com.packt.bookstore.gateway_server.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayRouteConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                // PredicateEvaluator + PreFilter + URI Resolution
+                .route("packt-inventory-service", r -> r
+                        .path("/packt/inventory/api/**") // PredicateEvaluator
+                        .filters(f -> f
+                                .rewritePath("/packt/inventory/api/(?<segment>.*)", "/inventory/api/${segment}") // PreFilter
+                                .addResponseHeader("X-Processed-By", "Spring-Gateway") // PostFilter
+                                .circuitBreaker(config -> config
+                                        .setName("inventoryCB")
+                                        .setFallbackUri("forward:/fallback/inventory")
+                                        .setRouteId("packt-inventory-service")
+                                        )
+                        )
+                        .uri("lb://inventory-service") // HandlerMapping - service discovery
+                )
+
+                .route("packt-user-service", r -> r
+                        .path("/packt/user/api/**")
+                        .filters(f -> f
+                                .rewritePath("/packt/user/api/(?<segment>.*)", "/user/api/${segment}"))
+                        .uri("lb://user-ms") // HandlerMapping
+                )
+
+                .build();
+    }
+
+}
